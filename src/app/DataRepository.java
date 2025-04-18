ae89f2492bc4148617e77af9dc28eb51c8bb8ab0
melmanss <cfhfyjdbx@gmail.com>
2025-04-18 18:40:36 +0300

    New commit

--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -4,10 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataRepository {
-    // Метод повертає список імен
-    public List<String> getData() {
-        List<String> list = ArrayList<>();
 
+    private final List<String> list;
+    // Метод повертає список імен
+    public DataRepository ()  {
+        list = new ArrayList<>();
+
+        list.add("Alice");
+        list.add("Bob");
+        list.add("Lucy");
+        list.add("Denis");
+        list.add("Tom");
+    }
+
+    public List<String> getData() {
         return list;
     }
 }
