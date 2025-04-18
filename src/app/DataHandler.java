9c947e3ed36394ca6ec13296ff615170baf4ae0c
melmanss <cfhfyjdbx@gmail.com>
2025-04-18 18:29:36 +0300

    First commit

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,4 +1,28 @@
 package app;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DataHandler {
+
+    // Метод формує виведення імені за певним індексом
+    public String formOutput(List<String> list, int index) {
+        try {
+            String name = list.
+            return "Name: " + name + " is in index " + index;
+        } catch (IndexOutOfBoundsException e) {
+            return "Wrong index!";
+        }
+    }
+
+    // Метод формує виведення нумерованого списку імен
+    public String formListOutput(List list) {
+        StringBuilder sb = new StringBuilder();
+        AtomicInteger count = new AtomicInteger(1);
+        for (name : ) {
+            sb.append(String.("%d) %s%n",
+                    count.getAndIncrement(), name));
+        }
+        return "\nNames:\n" + sb;
+    }
 }
--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -1,4 +1,13 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataRepository {
+    // Метод повертає список імен
+    public List<String> getData() {
+        List<String> list = ArrayList<>();
+
+        return list;
+    }
 }
